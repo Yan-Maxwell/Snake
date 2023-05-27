0baa7d3e3989aea521cf8968de3ec501e93b395c
Yan-Maxwell <134377176+Yan-Maxwell@users.noreply.github.com>
2023-05-27 16:36:19 +0800

    Add files via upload

--- snake_frame/src/main/java/cn/edu/sustech/cs110/snake/control/LoginController.java
+++ snake_frame/src/main/java/cn/edu/sustech/cs110/snake/control/LoginController.java
@@ -1,17 +1,18 @@
 package cn.edu.sustech.cs110.snake.control;
 
 import cn.edu.sustech.cs110.snake.Context;
-import cn.edu.sustech.cs110.snake.model.User;
+import cn.edu.sustech.cs110.snake.view.AdvancedStage;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+
+import java.io.PrintWriter;
 import java.util.Optional;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -30,57 +31,45 @@ public class LoginController {
         String password = passwordField.getText();
 
         try {
-            File usersFile = new File("users.txt");
+            File usersFile = new File(username+".txt");
+            //新用户则保存密码并进入home
             if (!usersFile.exists()) {
-                usersFile.createNewFile();
+                PrintWriter output = new PrintWriter(usersFile);
+                output.println(password);
+                output.close();
+
+                Context.INSTANCE.setCurrentUser(username);
+                new AdvancedStage("home.fxml")
+                        .withTitle("HOME")
+                        .shows();
+                login.getScene().getWindow().hide();
+
             }
-
-            Scanner in = new Scanner(usersFile);
-            boolean userExists = false;
-            boolean passwordCorrect = false;
-
-            while (in.hasNextLine()) {
-                String line = in.nextLine();
-                String[] userFields = line.split(",");
-                String existingUsername = userFields[0];
-                String existingPassword = userFields[1];
-
-                if (username.equals(existingUsername)) {
-                    userExists = true;
-                    if (password.equals(existingPassword)) {
-                        passwordCorrect = true;
-                        break;
+            //老用户则校对密码并弹alert或进入home
+            else if (usersFile.exists()) {
+                Scanner input = new Scanner(usersFile);
+                String correctPassword = input.nextLine();
+                if (!password.equals(correctPassword)) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Incorrect Password");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Your password is incorrect!");
+                    ButtonType backButton = new ButtonType("Back");
+                    alert.getButtonTypes().setAll(backButton);
+                    Optional<ButtonType> result = alert.showAndWait();
+                    if (result.isPresent() && result.get() == backButton) {
+                        return;
                     }
+                    input.close();
+                }
+                else {
+                    Context.INSTANCE.setCurrentUser(username);
+                    new AdvancedStage("home.fxml")
+                            .withTitle("HOME")
+                            .shows();
+                    login.getScene().getWindow().hide();
                 }
             }
-
-            in.close();
-
-            if (!userExists) {
-                // Create a new user
-                User newUser = new User(username, password);
-                // Append the new user to the file
-                FileWriter fileWriter = new FileWriter(usersFile, true);
-                fileWriter.write(newUser.toString() + "\n");
-                fileWriter.close();
-            }
-            else if (!passwordCorrect) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Incorrect Password");
-                alert.setHeaderText(null);
-                alert.setContentText("Your password is incorrect!");
-                ButtonType backButton = new ButtonType("Back");
-                alert.getButtonTypes().setAll(backButton);
-                Optional<ButtonType> result = alert.showAndWait();
-                if (result.isPresent() && result.get() == backButton) {
-                    return;
-                }
-                return;
-            }
-
-            Context.INSTANCE.setCurrentUser(username);
-            HomeController.showHomeScreen();
-            login.getScene().getWindow().hide();
 
         } catch (IOException e) {
             e.printStackTrace();
