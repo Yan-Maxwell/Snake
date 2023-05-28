9584a49933d5ec7badd1c9517e845f25ba5f2c60
sunfanisme <134377374+sunfanisme@users.noreply.github.com>
2023-05-28 14:33:22 +0800

    Add files via upload

---
+++ snake_frame/src/main/java/cn/edu/sustech/cs110/snake/model/Wall.java
@@ -0,0 +1,48 @@
+package cn.edu.sustech.cs110.snake.model;
+
+import cn.edu.sustech.cs110.snake.enums.Direction;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wall {
+    private final List<Position> wall = new ArrayList<>(15 * 15 / 2);
+
+    @java.lang.SuppressWarnings("all")
+    public List<Position> getWall() {
+        return this.wall;
+    }
+    public Wall(int map){
+        if(map==1){
+                for(int i =8; i<17; i++){
+                    wall.add(new Position(15,i));
+                }
+                for(int j =8; j<21; j++){
+                    wall.add(new Position(j,7));
+                }
+                for(int j =8; j<21; j++){
+                    wall.add(new Position(j,17));
+                }
+        }
+        else if (map==2) {
+                for(int i =8; i<21; i++){
+                    wall.add(new Position(i,5));
+                }
+                for(int j =6; j<19; j++){
+                    wall.add(new Position(20,j));
+                }
+        }
+        else{
+            for(int j=8; j<20;j++){
+                wall.add(new Position(7,j));
+            }
+            for(int i = 8; i<21;i++){
+                wall.add(new Position(i,15));
+            }
+        }
+    }
+
+    public Wall(){}
+}
