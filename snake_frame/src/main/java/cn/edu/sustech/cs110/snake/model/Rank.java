914abcef79f636b2fad5faf6a3fa0cc0eb63ec52
Yan-Maxwell <134377176+Yan-Maxwell@users.noreply.github.com>
2023-05-28 14:39:58 +0800

    Add files via upload

---
+++ snake_frame/src/main/java/cn/edu/sustech/cs110/snake/model/Rank.java
@@ -0,0 +1,12 @@
+package cn.edu.sustech.cs110.snake.model;
+
+public class Rank {
+    public static String s1;
+    public static String s2;
+    public static String s3;
+    public static int i1;
+    public static int i2;
+    public static int i3;
+
+
+}
